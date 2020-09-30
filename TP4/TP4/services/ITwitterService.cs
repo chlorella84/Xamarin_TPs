53de5c377b24ccd7406737fc7bffb5fc12f601a9
Dominika LE CUNFF <dominika.lecunff2019@campus-eni.fr>
2020-09-30 15:05:42 +0200

    tp4

---
+++ TP4/TP4/services/ITwitterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TP4.models;
+
+namespace TP4.services
+{
+    public interface ITwitterService
+    {
+        bool authenticate(String key, String secret);
+        List<Tweet> getTweets();
+    }
+}
