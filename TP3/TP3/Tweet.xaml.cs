98fb26f195e4460892e39336c5004af92c4e02e9
Dominika LE CUNFF <dominika.lecunff2019@campus-eni.fr>
2020-09-30 11:44:00 +0200

    tp3 minor changes

--- TP3/TP3/Tweet.xaml.cs
+++ TP3/TP3/Tweet.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace TP3
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Tweet : ContentPage
+    public partial class Tweet : ContentView
     {
         public Tweet()
         {
